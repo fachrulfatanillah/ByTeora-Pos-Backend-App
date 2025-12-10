4a08cf7d67aa624c3141d4a3e2395ea4b4904b88
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-12-11 02:28:16 +0700

    membuat package route

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	
+	"ByTeora-Pos-Backend-App/route"
+)
 
 func main() {
-  router := gin.Default()
-  router.GET("/ping", func(c *gin.Context) {
-    c.JSON(200, gin.H{
-      "message": "pong",
-    })
-  })
-  router.Run() // listens on 0.0.0.0:8080 by default
+	r := gin.Default()
+
+	route.RegisterRoutes(r)
+
+	r.Run(":8080")
 }
