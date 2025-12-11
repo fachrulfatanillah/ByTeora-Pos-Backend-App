b569b72f6ae50bee8360c0f8bd05a4e2c607738d
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-12-11 16:34:27 +0700

    membuat migrasi table store

--- config/migration.go
+++ config/migration.go
@@ -5,7 +5,12 @@ import (
 )
 
 func RunMigrations() {
-	userTable := `
+	MigrateUserTable()
+	MigrateStoreTable()
+}
+
+func MigrateUserTable() {
+	query := `
 	CREATE TABLE IF NOT EXISTS user (
 		id INT AUTO_INCREMENT PRIMARY KEY,
 		uuid VARCHAR(36) NOT NULL UNIQUE,
@@ -22,10 +27,36 @@ func RunMigrations() {
 	);
 	`
 
-	_, err := DB.Exec(userTable)
+	_, err := DB.Exec(query)
 	if err != nil {
 		log.Fatal("Migration failed:", err)
 	}
 
 	log.Println("Database migration completed!")
+}
+
+func MigrateStoreTable() {
+	query := `
+	CREATE TABLE IF NOT EXISTS store (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		uuid VARCHAR(36) NOT NULL UNIQUE,
+		user_id INT NOT NULL,
+		store_name VARCHAR(150) NOT NULL,
+		address TEXT NULL,
+		phone_number VARCHAR(20) NULL,
+		status VARCHAR(20) NOT NULL DEFAULT 'active' CHECK (status IN ('active', 'inactive')),
+		deleted_at TIMESTAMP NULL,
+		created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		
+		CONSTRAINT fk_store_user FOREIGN KEY (user_id) REFERENCES user(id)
+	) ENGINE=InnoDB;
+	`
+
+	_, err := DB.Exec(query)
+	if err != nil {
+		log.Fatal("Failed to migrate store table:", err)
+	}
+
+	log.Println("Store table migrated successfully!")
 }
